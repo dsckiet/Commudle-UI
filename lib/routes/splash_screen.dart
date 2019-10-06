47343e1b0ce2d782a6f18e206fd70eb6d8fe349c
shubham100ev <shubhamsingh19981110@gmail.com>
2019-10-06 14:20:21 +0530

    Splash Edit

--- lib/routes/splash_screen.dart
+++ lib/routes/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => new _SplashScreenState();
@@ -9,6 +8,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   double _opacity = 1.0;
+
   startTime() async {
     var _duration = new Duration(milliseconds: 3000);
     return new Timer(_duration, navigationPage);
@@ -21,43 +21,41 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-
   }
 
   @override
-  Widget build(BuildContext context) {return Scaffold(
-    body: Stack(fit: StackFit.expand, children: <Widget>[
-      Padding(
-        padding: EdgeInsets.all(120),
-        child: Image(
-          image: AssetImage('assets/images/commudle-logo.png'),
-        ),
-      ),
-      Container(
-        child: GestureDetector(
-          child: AnimatedOpacity(
-            duration: Duration(milliseconds: 2600),
-            opacity: this._opacity,
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        const Color(0xFF4F73FF),
-                        const Color(0xFF28A6FF)
-                      ])),
-            ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(fit: StackFit.expand, children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(120),
+          child: Image(
+            image: AssetImage('assets/images/commudle-logo.png'),
           ),
-          onTap: () {
-            setState(() => this._opacity = 1.0 - this._opacity);
-            startTime();
-          },
         ),
-      ),
-
-    ]),
-  );
+        Container(
+          child: GestureDetector(
+            child: AnimatedOpacity(
+              duration: Duration(milliseconds: 2600),
+              opacity: this._opacity,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                      const Color(0xFF4F73FF),
+                      const Color(0xFF28A6FF)
+                    ])),
+              ),
+            ),
+            onTap: () {
+              setState(() => this._opacity = 1.0 - this._opacity);
+              startTime();
+            },
+          ),
+        ),
+      ]),
+    );
   }
 }
-
